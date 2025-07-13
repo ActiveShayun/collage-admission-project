56bdb75314a008ba5a9e24d9bd499a4f921d8049
ActiveShayun <wevdeveloper0477gmail.com>
2025-07-13 10:28:43 +0600

    add env file fow production

--- src/app/components/shared/axiosHooks/axiosPublic.js
+++ src/app/components/shared/axiosHooks/axiosPublic.js
@@ -1,10 +1,10 @@
 'use client'
 import axios from 'axios';
 const instant = axios.create({
-    baseURL: ''
+    baseURL: 'http://localhost:3000'
 })
 const axiosPublic = () => {
-    return axios
+    return instant
 };
 
 export default axiosPublic;
